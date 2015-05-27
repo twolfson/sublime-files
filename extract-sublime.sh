cd4b3659a9f73abb407a5fb0e638ec3ff5ef52b6
Todd Wolfson <todd@twolfson.com>
2015-05-27 03:00:36 -0500

    Completed extraction script

--- extract-sublime.sh
+++ extract-sublime.sh
@@ -43,3 +43,41 @@ for file in *; do
     fi
   fi
 done
+
+# For each of the Sublime packages
+cd Packages/
+for file in *; do
+  # Extract the packages to its corresponding folder
+  #   e.g. `ActionScript.sublime-package` -> `ActionScript`
+  pkg_dir="$(echo "$file" | sed -E "s/.sublime-package//")"
+  unzip -d "$pkg_dir" "$file";
+
+  # Remove the original file
+  rm "$file"
+done
+cd ../
+
+# Perform a sanity check that no single file is over 1MB
+shopt -s globstar
+for file in **/*; do
+  # If it's a file (e.g. not a folder/symlink)
+  if test -f "$file"; then
+    # Get its size
+    #  `du $file | cut --fields 1`: `4172 plugin_host` -> `4172` (kb)
+    filesize="$(du "$file" | cut --fields 1)"
+
+    # If it's over 1MB, then complain about it
+    if test "$filesize" -ge 1024; then
+      echo "WARNING: $file is over 1MB large" 1>&2
+    fi
+  fi
+done
+shopt -u globstar
+
+# Transfer our files
+#   `ls` -> `sublime_text_3 sublime_text_3_build_3083_x64.tar.bz2`
+cd ../
+cp sublime_text_3/* ../ -R
+
+# Navigate out of our temporary directory and notify the user that we are done
+echo "Extraction complete. Please run \`git status\` to see what has changed." 1>&2
