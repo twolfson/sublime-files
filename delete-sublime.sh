c2aaf94d8dca953ad3098405953ee771970eed7f
Todd Wolfson <todd@twolfson.com>
2016-02-09 23:56:59 -0600

    Added deletion script

---
+++ delete-sublime.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Exit on first failing command
+set -e
+
+# List all the files in our directory
+for file in *; do
+  # If the file is reserved, then skip it
+  if echo "$file" | grep -E "^(.git|.gitignore|CHANGELOG.md|README.md|UNLICENSE|delete-sublime.sh|download-eula.sh|extract-sublime.sh)$" &> /dev/null; then
+    continue
+  fi
+
+  # Otherwise, remove it
+  rm -r "$file"
+done
+
+# Notify the user that we are done
+echo "Deletion complete. Please run \`git status\` to see what has changed." 1>&2
